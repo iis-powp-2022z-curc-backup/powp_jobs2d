b271d8219a96b38fe223e4410e43e8722e2e88ac
Dudzinski-Robert <62355844+Dudzinski-Robert@users.noreply.github.com>
2022-11-20 19:08:45 +0100

    Fix line drawing in adapter

--- src/main/java/edu/kis/powp/jobs2d/drivers/adapter/Job2dAdapter.java
+++ src/main/java/edu/kis/powp/jobs2d/drivers/adapter/Job2dAdapter.java
@@ -29,6 +29,7 @@ public class Job2dAdapter extends DrawPanelController implements Job2dDriver {
 		ILine line = LineFactory.getBasicLine();
 		line.setStartCoordinates(this.startX, this.startY);
 		line.setEndCoordinates(x, y);
+		setPosition(x,y);
 
 		drawPanelController.drawLine(line);
 	}
