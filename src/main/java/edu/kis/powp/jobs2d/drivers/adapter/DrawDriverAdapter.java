352bc5bf6bcad0212cf309d271a45ad540f2945b
p-olszewski <przemo1297@gmail.com>
2022-11-20 19:37:48 +0100

    LAB1 Fix display in the right window

--- src/main/java/edu/kis/powp/jobs2d/drivers/adapter/DrawDriverAdapter.java
+++ src/main/java/edu/kis/powp/jobs2d/drivers/adapter/DrawDriverAdapter.java
@@ -34,6 +34,6 @@ public class DrawDriverAdapter extends DrawPanelController implements Job2dDrive
 
 	@Override
 	public String toString() {
-		return "@Q!$!@$!#@$(*#@&Q(%^*#@";
+		return "DrawDriverAdapter";
 	}
 }
