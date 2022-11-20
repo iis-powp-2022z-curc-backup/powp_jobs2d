6adc204961e07fad653f4467f886cd1a0a984f33
Bartłomiej Ołubek <bartek.olubek@gmail.com>
2022-11-20 18:39:22 +0100

    [3.2.0] Add drawing inside DrawController window

--- src/main/java/edu/kis/powp/jobs2d/drivers/adapter/DrawerJobs2dAdapter.java
+++ src/main/java/edu/kis/powp/jobs2d/drivers/adapter/DrawerJobs2dAdapter.java
@@ -1,9 +1,11 @@
 package edu.kis.powp.jobs2d.drivers.adapter;
 
+import edu.kis.legacy.drawer.panel.DefaultDrawerFrame;
 import edu.kis.legacy.drawer.panel.DrawPanelController;
 import edu.kis.legacy.drawer.shape.ILine;
 import edu.kis.legacy.drawer.shape.LineFactory;
 import edu.kis.powp.jobs2d.Job2dDriver;
+import edu.kis.powp.jobs2d.features.DrawerFeature;
 
 /**
  * driver adapter to drawer with several bugs.
@@ -23,11 +25,14 @@ public class DrawerJobs2dAdapter extends DrawPanelController implements Job2dDri
 
 	@Override
 	public void operateTo(int x, int y) {
+		DrawPanelController drawPanelController = DrawerFeature.getDrawerController();
+		DefaultDrawerFrame.getDefaultDrawerFrame().setVisible(true);
+
 		ILine line = LineFactory.getBasicLine();
 		line.setStartCoordinates(this.startX, this.startY);
 		line.setEndCoordinates(x, y);
 
-		drawLine(line);
+		drawPanelController.drawLine(line);
 	}
 
 	@Override
