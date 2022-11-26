c310dd385a0ac339193bc8af24390b1efbbe4d18
229258 <229258@edu.p.lodz.pl>
2022-11-26 14:30:42 +0100

    Command 3.1.1 - Template of classes hierarchy.

---
+++ src/main/java/edu/kis/powp/jobs2d/command/Job2dDriver.java
@@ -0,0 +1,6 @@
+package edu.kis.powp.jobs2d.command;
+
+public interface Job2dDriver {
+	public void setPosition(int x, int y);
+	public void operateTo(int x, int y);
+}
---
+++ src/main/java/edu/kis/powp/jobs2d/command/SetPositionCommand.java
@@ -0,0 +1,18 @@
+package edu.kis.powp.jobs2d.command;
+
+public class SetPositionCommand implements DriverCommand {
+	private int x;
+	private int y;
+
+	public SetPositionCommand(int x, int y) {
+		super();
+		this.x = x;
+		this.y = y;
+	}
+	
+	@Override
+	public void execute() {
+		// TODO Auto-generated method stub
+		
+	}
+}
