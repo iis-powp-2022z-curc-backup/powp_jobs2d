0665ac5286dac3c9b3e5de5d43d8c1c1a78aa62e
Hubertius <hubert.kopec@interia.pl>
2022-11-26 14:28:51 +0100

    Lab2-3.1.3 Implementation of ComplexCommand

---
+++ src/main/java/edu/kis/powp/jobs2d/commands/ComplexCommand.java
@@ -0,0 +1,32 @@
+package edu.kis.powp.jobs2d.commands;
+
+import edu.kis.powp.jobs2d.Job2dDriver;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ComplexCommand implements DriverCommand{
+
+    private List<DriverCommand> commandsList;
+
+    public ComplexCommand() {
+        this.commandsList = new ArrayList<>();
+    }
+
+    public ComplexCommand(List<DriverCommand> existingCommandsList) {
+        this.commandsList = new ArrayList<>(existingCommandsList);
+    }
+
+    public void addCommand(DriverCommand newCommandToAdd) {
+        this.commandsList.add(newCommandToAdd);
+    }
+
+    public void addCommands(List<DriverCommand> existingCommandsList) {
+        this.commandsList.addAll(existingCommandsList);
+    }
+
+    @Override
+    public void execute(Job2dDriver driver) {
+    
+    }
+}
