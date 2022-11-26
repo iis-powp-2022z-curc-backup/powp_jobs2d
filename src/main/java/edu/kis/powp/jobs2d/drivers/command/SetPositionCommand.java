765b432c8b87dbccf89526311451e04dbd1d6399
kamilmichalskiya <kamilmichalskiya@users.noreply.github.com>
2022-11-26 13:58:21 +0100

    3.1.1 Create Driver Command interface

---
+++ src/main/java/edu/kis/powp/jobs2d/drivers/command/SetPositionCommand.java
@@ -0,0 +1,11 @@
+package edu.kis.powp.jobs2d.drivers.command;
+
+public class SetPositionCommand implements DriverCommand {
+    private int x = 0;
+    private int y = 0;
+
+    @Override
+    public void execute() {
+
+    }
+}
