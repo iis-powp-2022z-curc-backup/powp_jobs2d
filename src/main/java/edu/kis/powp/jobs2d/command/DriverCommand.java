6f48521b480b846c3802d12151fe41719b7350e0
Artur <artur@debian-BULLSEYE-live-builder-AMD64>
2022-11-26 14:11:10 +0100

    DriverCommand interface creation

---
+++ src/main/java/edu/kis/powp/jobs2d/command/DriverCommand.java
@@ -0,0 +1,8 @@
+package edu.kis.powp.jobs2d.command;
+
+import javax.swing.*;
+
+public interface DriverCommand {
+    void execute();
+
+}
--- src/main/java/edu/kis/powp/jobs2d/command/Jobs2dDriver.java
+++ src/main/java/edu/kis/powp/jobs2d/command/Jobs2dDriver.java
@@ -3,4 +3,5 @@ package edu.kis.powp.jobs2d.command;
 public interface Jobs2dDriver {
     void set_Position(int x, int y);
     void operateTo(int x, int y);
+
 }
