429891fe8b719b29c7195f3048e33cbfb87bdcaf
Artur <artur@debian-BULLSEYE-live-builder-AMD64>
2022-11-26 14:15:25 +0100

    SetPositionCommand creation

---
+++ src/main/java/edu/kis/powp/jobs2d/command/SetPositionCommand.java
@@ -0,0 +1,12 @@
+package edu.kis.powp.jobs2d.command;
+
+public class SetPositionCommand implements DriverCommand{
+    int x;
+    int y;
+
+
+    @Override
+    public void execute() {
+        
+    }
+}
