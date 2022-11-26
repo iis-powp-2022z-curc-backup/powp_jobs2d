35415d3aa1cb52bd6509d79a80326c4924e5f734
kamilmichalskiya <kamilmichalskiya@users.noreply.github.com>
2022-11-26 14:11:03 +0100

    3.1.2 Add Driver Command implementation

--- src/main/java/edu/kis/powp/jobs2d/drivers/command/OperateToCommand.java
+++ src/main/java/edu/kis/powp/jobs2d/drivers/command/OperateToCommand.java
@@ -1,11 +1,18 @@
 package edu.kis.powp.jobs2d.drivers.command;
 
+import edu.kis.powp.jobs2d.Job2dDriver;
+
 public class OperateToCommand implements DriverCommand {
-    private int x = 0;
-    private int y = 0;
+    private final int x;
+    private final int y;
+
+    public OperateToCommand(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
 
     @Override
-    public void execute() {
-
+    public void execute(Job2dDriver driver) {
+        driver.operateTo(x, y);
     }
 }
