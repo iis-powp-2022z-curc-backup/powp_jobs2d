029f1a1a0f28064ba3eeef07cfe6db5593f227f0
Justyna Owczarek <229301@edu.p.lodz.pl>
2022-11-30 23:04:59 +0100

    3.2.2 Draw inside drawArea instead of separate window

--- src/main/java/edu/kis/powp/jobs2d/drivers/adapter/DrawerToJobs2dMagicAdapter.java
+++ src/main/java/edu/kis/powp/jobs2d/drivers/adapter/DrawerToJobs2dMagicAdapter.java
@@ -1,20 +1,21 @@
 package edu.kis.powp.jobs2d.drivers.adapter;
 
-import edu.kis.legacy.drawer.panel.DefaultDrawerFrame;
 import edu.kis.legacy.drawer.panel.DrawPanelController;
 import edu.kis.legacy.drawer.shape.ILine;
 import edu.kis.legacy.drawer.shape.LineFactory;
 import edu.kis.powp.jobs2d.Job2dDriver;
-import edu.kis.powp.jobs2d.features.DrawerFeature;
+
+import javax.swing.*;
 
 /**
- * driver adapter to drawer with several bugs.
+ * Drawer to Jobs2dMagic adapter
  */
 public class DrawerToJobs2dMagicAdapter extends DrawPanelController implements Job2dDriver {
 	private int startX = 0, startY = 0;
 
-	public DrawerToJobs2dMagicAdapter() {
+	public DrawerToJobs2dMagicAdapter(JPanel drawArea) {
 		super();
+		initialize(drawArea);
 	}
 
 	@Override
@@ -25,11 +26,10 @@ public class DrawerToJobs2dMagicAdapter extends DrawPanelController implements J
 
 	@Override
 	public void operateTo(int x, int y) {
-		DrawPanelController controller = DrawerFeature.getDrawerController();
 		ILine line = LineFactory.getBasicLine();
 		line.setStartCoordinates(this.startX, this.startY);
 		line.setEndCoordinates(x, y);
-		controller.drawLine(line);
+		drawLine(line);
 		setPosition(x, y);
 	}
 
