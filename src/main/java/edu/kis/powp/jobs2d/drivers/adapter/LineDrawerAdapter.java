2e496ce70dbfd5078e7200db329bec82edc8107b
Artur229322 <229322@edu.p.lodz.pl>
2022-11-30 21:49:50 +0100

    Added implementation of Job2dDriver methods.

--- src/main/java/edu/kis/powp/jobs2d/drivers/adapter/LineDrawerAdapter.java
+++ src/main/java/edu/kis/powp/jobs2d/drivers/adapter/LineDrawerAdapter.java
@@ -1,4 +1,33 @@
 package edu.kis.powp.jobs2d.drivers.adapter;
 
-public class LineDrawerAdapter {
+import edu.kis.legacy.drawer.panel.DrawPanelController;
+import edu.kis.legacy.drawer.shape.ILine;
+import edu.kis.legacy.drawer.shape.LineFactory;
+import edu.kis.powp.jobs2d.Job2dDriver;
+
+
+import static edu.kis.powp.jobs2d.features.DrawerFeature.getDrawerController;
+
+public class LineDrawerAdapter implements Job2dDriver {
+    private int startX = 0, startY = 0;
+
+    public LineDrawerAdapter() {
+        super();
+    }
+
+    @Override
+    public void setPosition(int x, int y) {
+        this.startX = x;
+        this.startY = y;
+    }
+
+    @Override
+    public void operateTo(int x, int y) {
+        ILine line = LineFactory.getSpecialLine();
+        line.setStartCoordinates(this.startX, this.startY);
+        line.setEndCoordinates(x, y);
+        DrawPanelController drawerController = getDrawerController();
+        drawerController.drawLine(line);
+        setPosition(x,y);
+    }
 }
