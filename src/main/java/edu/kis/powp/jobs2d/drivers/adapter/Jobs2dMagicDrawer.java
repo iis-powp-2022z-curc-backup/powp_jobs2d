15551758d09b181e940a7d3c62197d1a657629ea
Czarek Karczewski <keracz1997@gmail.com>
2022-11-30 20:15:42 +0100

    fix drawer

--- src/main/java/edu/kis/powp/jobs2d/drivers/adapter/Jobs2dMagicDrawer.java
+++ src/main/java/edu/kis/powp/jobs2d/drivers/adapter/Jobs2dMagicDrawer.java
@@ -4,7 +4,6 @@ import edu.kis.legacy.drawer.panel.DrawPanelController;
 import edu.kis.legacy.drawer.shape.ILine;
 import edu.kis.legacy.drawer.shape.LineFactory;
 import edu.kis.powp.jobs2d.Job2dDriver;
-import edu.kis.legacy.drawer.panel.DefaultDrawerFrame;
 import edu.kis.powp.jobs2d.features.DrawerFeature;
 /**
  * driver adapter to drawer with several bugs.
@@ -24,13 +23,13 @@ public class Jobs2dMagicDrawer extends DrawPanelController implements Job2dDrive
 
 	@Override
 	public void operateTo(int x, int y) {
-		DrawPanelController drawPanelController = DrawerFeature.getDrawerController();
-		DefaultDrawerFrame.getDefaultDrawerFrame().setVisible(true);
+
 		ILine line = LineFactory.getBasicLine();
 		line.setStartCoordinates(this.startX, this.startY);
 		line.setEndCoordinates(x, y);
 
-		drawPanelController.drawLine(line);
+//		drawPanelController.drawLine(line);
+		DrawerFeature.getDrawerController().drawLine(line);
 	}
 
 	@Override
