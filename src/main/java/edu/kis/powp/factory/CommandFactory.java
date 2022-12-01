e8431f23e0d1a63ba7f4943b0056424c78d9c281
Justyna Owczarek <229301@edu.p.lodz.pl>
2022-12-01 23:26:37 +0100

    3.1.5 Add CommandFactory with rectangle & triangle drawings

--- src/main/java/edu/kis/powp/factory/CommandFactory.java
+++ src/main/java/edu/kis/powp/factory/CommandFactory.java
@@ -1,28 +1,30 @@
 package edu.kis.powp.factory;
 
+import edu.kis.powp.command.ComplexCommand;
 import edu.kis.powp.command.DriverCommand;
 import edu.kis.powp.command.OperateToCommand;
 import edu.kis.powp.command.SetPositionCommand;
-import edu.kis.powp.jobs2d.Job2dDriver;
 
 import java.util.ArrayList;
 import java.util.List;
 
 public class CommandFactory {
-    public List<DriverCommand> getCommand(Job2dDriver driver, String command) {
-        List<DriverCommand> commands = new ArrayList<DriverCommand>();
-        switch(command) {
-            case "rectangle":
-                commands.add(new SetPositionCommand(driver, 0, 0));
-                commands.add(new OperateToCommand(driver, 0, 0));
-                return commands;
+    public static DriverCommand getDrawRectangleCommand() {
+        List<DriverCommand> commands = new ArrayList<>();
+        commands.add(new SetPositionCommand( -100, -50));
+        commands.add(new OperateToCommand( 200, -50));
+        commands.add(new OperateToCommand( 200, 100));
+        commands.add(new OperateToCommand( -100, 100));
+        commands.add(new OperateToCommand( -100, -50));
+        return new ComplexCommand(commands);
+    }
 
-            case "circle":
-                commands.add(new SetPositionCommand(driver, 0, 0));
-                return commands;
-
-            default:
-                return new ArrayList<>();
-        }
+    public static DriverCommand getDrawTriangleCommand() {
+        List<DriverCommand> commands = new ArrayList<>();
+        commands.add(new SetPositionCommand(0, 100));
+        commands.add(new OperateToCommand(100, -100));
+        commands.add(new OperateToCommand(-100, -100));
+        commands.add(new OperateToCommand(0, 100));
+        return new ComplexCommand(commands);
     }
 }
