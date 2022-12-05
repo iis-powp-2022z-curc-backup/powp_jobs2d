09c5e28c259068cb3fc334571b8e08db501a4872
229258 <229258@edu.p.lodz.pl>
2022-12-05 22:47:45 +0100

    Added implementation of Driver Command.

--- src/main/java/edu/kis/powp/command/OperateToCommand.java
+++ src/main/java/edu/kis/powp/command/OperateToCommand.java
@@ -1,5 +1,7 @@
 package edu.kis.powp.command;
 
+import edu.kis.powp.jobs2d.Job2dDriver;
+
 public class OperateToCommand implements DriverCommand {
 
 	private int x;
@@ -12,7 +14,7 @@ public class OperateToCommand implements DriverCommand {
 	}
 
 	@Override
-	public void execute() {
-		// TODO Auto-generated method stub
+	public void execute(Job2dDriver driver) {
+		driver.setPosition(x, y);
 	}
 }
--- src/main/java/edu/kis/powp/command/SetPositionCommand.java
+++ src/main/java/edu/kis/powp/command/SetPositionCommand.java
@@ -1,5 +1,7 @@
 package edu.kis.powp.command;
 
+import edu.kis.powp.jobs2d.Job2dDriver;
+
 public class SetPositionCommand implements DriverCommand {
 
 	private int x;
@@ -12,7 +14,7 @@ public class SetPositionCommand implements DriverCommand {
 	}
 
 	@Override
-	public void execute() {
-		// TODO Auto-generated method stub
+	public void execute(Job2dDriver driver) {
+		driver.setPosition(x, y);
 	}
 }
