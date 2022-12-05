6251f9429cf80af945f48f4ac0bd5487f6fa6bf6
pawel <pawelpatyk@interia.com>
2022-12-05 14:26:24 +0100

    Ex 3.1.1

---
+++ src/main/java/edu/kis/powp/jobs2d/command/OperateToCommand.java
@@ -0,0 +1,12 @@
+package edu.kis.powp.jobs2d.command;
+
+public class OperateToCommand implements DriverCommand{
+    private int x;
+    private int y;
+	@Override
+	public void execute() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
