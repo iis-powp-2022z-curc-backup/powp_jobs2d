2a10de565573ce7a4f36b6fd9a91a1dad6824bc7
DanielWap <daniel.waprzko@outlook.com>
2022-12-09 23:38:08 +0100

    Lab 1 3.2.1 Fixed method to draw inside window
    Lab 1 3.2.1 Fixed method to draw inside window

--- src/main/java/edu/kis/powp/jobs2d/drivers/adapter/Job2dAdapter.java
+++ src/main/java/edu/kis/powp/jobs2d/drivers/adapter/Job2dAdapter.java
@@ -4,13 +4,11 @@ import edu.kis.legacy.drawer.panel.DrawPanelController;
 import edu.kis.legacy.drawer.shape.ILine;
 import edu.kis.legacy.drawer.shape.LineFactory;
 import edu.kis.powp.jobs2d.Job2dDriver;
+import edu.kis.legacy.drawer.panel.DefaultDrawerFrame;
+import edu.kis.powp.jobs2d.features.DrawerFeature;
 
-/**
- * driver adapter to drawer with several bugs.
- */
 public class Job2dAdapter extends DrawPanelController implements Job2dDriver {
 	private int startX = 0, startY = 0;
-
 	public Job2dAdapter() {
 		super();
 	}
@@ -23,11 +21,13 @@ public class Job2dAdapter extends DrawPanelController implements Job2dDriver {
 
 	@Override
 	public void operateTo(int x, int y) {
+		DrawPanelController controller = DrawerFeature.getDrawerController();
+		DefaultDrawerFrame.getDefaultDrawerFrame().setVisible(true);
 		ILine line = LineFactory.getBasicLine();
 		line.setStartCoordinates(this.startX, this.startY);
 		line.setEndCoordinates(x, y);
 
-		drawLine(line);
+		controller.drawLine(line);
 	}
 
 	@Override
