4512aebf4fe0b57d7d4626d38428c8cfe9a6ce31
DanielWap <daniel.waprzko@outlook.com>
2022-12-10 00:38:26 +0100

    Lab 2 3.1.1 Add command hierarchy classes
    Lab 2 3.1.1 Add command hierarchy classes

---
+++ src/main/java/edu/kis/powp/jobs2d/commands/SetPositionCommand.java
@@ -0,0 +1,17 @@
+package edu.kis.powp.jobs2d.commands;
+
+public class SetPositionCommand implements DriverCommand {
+
+    int x = 0;
+    int y = 0;
+
+    public SetPositionCommand(int x, int y){
+        this.x = x;
+        this.y = y;
+    }
+
+    @Override
+    public void execute() {
+
+    }
+}
