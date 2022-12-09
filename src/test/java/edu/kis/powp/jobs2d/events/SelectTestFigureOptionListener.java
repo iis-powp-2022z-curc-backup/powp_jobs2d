cdb3428419a9187a238028897698a5ae75f3798a
DanielWap <daniel.waprzko@outlook.com>
2022-12-09 23:58:08 +0100

    Lab 3.2.3 Add Figure Joe 2 test  drawing figure
    Lab 3.2.3 Add Figure Joe 2 test  drawing figure

--- src/test/java/edu/kis/powp/jobs2d/events/SelectTestFigureOptionListener.java
+++ src/test/java/edu/kis/powp/jobs2d/events/SelectTestFigureOptionListener.java
@@ -5,6 +5,8 @@ import java.awt.event.ActionListener;
 
 import edu.kis.powp.jobs2d.drivers.DriverManager;
 import edu.kis.powp.jobs2d.magicpresets.FiguresJoe;
+import edu.kis.powp.jobs2d.features.DrawerFeature;
+
 
 public class SelectTestFigureOptionListener implements ActionListener {
 
@@ -16,6 +18,15 @@ public class SelectTestFigureOptionListener implements ActionListener {
 
 	@Override
 	public void actionPerformed(ActionEvent e) {
-		FiguresJoe.figureScript1(driverManager.getCurrentDriver());
+		DrawerFeature.getDrawerController().clearPanel();
+
+		switch (e.getActionCommand()){
+			case "Figure Joe 1":
+				FiguresJoe.figureScript1(driverManager.getCurrentDriver());
+				break;
+			case "Figure Joe 2":
+				FiguresJoe.figureScript2(driverManager.getCurrentDriver());
+				break;
+		}
 	}
 }
