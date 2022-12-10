09d73fc8128391fddd83ec9b06c503888cdbb856
DanielWap <daniel.waprzko@outlook.com>
2022-12-10 12:39:59 +0100

    Lab 3 Additional improvement - comments in DrawSquare and DrawRectangle
    Lab 3 Additional improvement - comments in DrawSquare and DrawRectangle

--- src/main/java/edu/kis/powp/jobs2d/factories/DrawRectangle.java
+++ src/main/java/edu/kis/powp/jobs2d/factories/DrawRectangle.java
@@ -11,18 +11,16 @@ public class DrawRectangle implements FactoryManager{
 	@Override
 	public DriverCommand create() {
 		ArrayList<DriverCommand> list = new ArrayList<>();
-		list.add(new SetPositionCommand(-100, -200));
-		list.add(new OperateToCommand(100, -200));
-		list.add(new SetPositionCommand(100, -200));
-		list.add(new OperateToCommand(100, 200));
-		list.add(new SetPositionCommand(100, 200));
-		list.add(new OperateToCommand(-100, 200));
-		list.add(new SetPositionCommand(-100, 200));
-		list.add(new OperateToCommand(-100, -200));
+		list.add(new SetPositionCommand(-100, -200)); // Point 1
+		list.add(new OperateToCommand(100, -200)); //Draw line to Point 2
+		list.add(new SetPositionCommand(100, -200)); //  Point 2
+		list.add(new OperateToCommand(100, 200)); //Draw line to Point 3
+		list.add(new SetPositionCommand(100, 200)); // Point 3
+		list.add(new OperateToCommand(-100, 200)); // Draw line to Point 4
+		list.add(new SetPositionCommand(-100, 200)); // Point 4
+		list.add(new OperateToCommand(-100, -200)); //Draw line to Point 1
 
 		ComplexCommand command = new ComplexCommand(list);
-
 		return command;
 	}
-
 }
--- src/main/java/edu/kis/powp/jobs2d/factories/DrawSquare.java
+++ src/main/java/edu/kis/powp/jobs2d/factories/DrawSquare.java
@@ -12,14 +12,14 @@ public class DrawSquare implements FactoryManager{
 	@Override
 	public DriverCommand create() {
 		ArrayList<DriverCommand> list = new ArrayList<>();
-		list.add(new SetPositionCommand(-50, -50));
-		list.add(new OperateToCommand(50, -50));
-		list.add(new SetPositionCommand(50, -50));
-		list.add(new OperateToCommand(50, 50));
-		list.add(new SetPositionCommand(50, 50));
-		list.add(new OperateToCommand(-50, 50));
-		list.add(new SetPositionCommand(-50, 50));
-		list.add(new OperateToCommand(-50, -50));
+		list.add(new SetPositionCommand(-50, -50)); // Point 1
+		list.add(new OperateToCommand(50, -50)); //Draw line to Point 2
+		list.add(new SetPositionCommand(50, -50)); // Point 2
+		list.add(new OperateToCommand(50, 50)); //Draw line to Point 3
+		list.add(new SetPositionCommand(50, 50)); // Point 3
+		list.add(new OperateToCommand(-50, 50)); //Draw line to Point 4
+		list.add(new SetPositionCommand(-50, 50)); // Point 4
+		list.add(new OperateToCommand(-50, -50)); //Draw line to Point 1
 
 		ComplexCommand command = new ComplexCommand(list);
 
